c5b23949b1d759d2c96bbd04b60986172c2f1032
Gabriel Costa <gubsocosta@gmail.com>
2025-02-19 21:45:30 -0300

    refactor: remove global variable

--- scripts.js
+++ scripts.js
@@ -1,22 +1,7 @@
-var todos = [
-  {
-    text: 'Aprender HTML, CSS e Javascript',
-    done: true
-  },
-  {
-    text: 'Aprender o básico de Vue JS',
-    done: false
-  },
-  {
-    text: 'Completar o desafio de Vue JS com excelência',
-    done: false
-  }
-];
-
 const todoApp = {
   data() {
     return {
-      todos: window.todos,
+      todos: [],
       newTodo: {
         done: false
       }
